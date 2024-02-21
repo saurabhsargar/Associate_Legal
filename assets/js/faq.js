86dd6960cee725cc6a8bc1d95675fb2c5c8eb899
saurabhsargar <saurabhsargar1234@gmail.com>
2024-02-21 10:32:19 +0530

    first commit

---
+++ assets/js/faq.js
@@ -0,0 +1,8 @@
+function toggleAnswer(question) {
+    let answer = question.nextElementSibling;
+    if (answer.style.display === "block") {
+        answer.style.display = "none";
+    } else {
+        answer.style.display = "block";
+    }
+}
